9183f36cd7ddfc6f9c9f44f157bba95dda85179b
Jason Cemra <cemrajc@users.noreply.github.com>
2016-08-06 14:30:51 +1000

    Added fixtures and test logic

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ var math = require('mathjax-node/lib/mj-page.js'),
     _ = require('lodash'),
     gutil = require('gulp-util'),
     through = require('through2'),
-    PluginError = gutil.pluginError;
+    PluginError = gutil.PluginError;
 
 const PLUGIN_NAME = "gulp-mathjax-node";
-math.start();
 
 
 /*
@@ -52,8 +51,11 @@ function indexAndLength(string, regex) {
     return result;
 }
 
-function renderHTML(_options) {
-    var error_html = "<h1 style='width: 100%;text-align:center;font-family:Arial,sans-serif;'>If you're seeing this, a major error has occurred. Please file an issue at the <a style='color:#67B1E5;' href='https://github.com/cemrajc/gulp-mathjax-node/issues'><b>gulp-mathjax-node</b></a> Github page!</h1>";
+function parseOptions(_options) {
+
+    if (typeof(_options) !== "object") {
+        throw new PluginError(PLUGIN_NAME, "Options must be of type 'object', not \'" + typeof(_options) + "\'");
+    }
 
     /*
     *
@@ -62,39 +64,52 @@ function renderHTML(_options) {
     *         [Supported] options should work as intended. See the README for more.
     *
     */
-    var options = _.defaults(_options, {
-        ex: 6,                          // ex-size in pixels [Not Supported]
-        width: 1,                       // width of container (in ex) for linebreaking and tags [Not Supported]
-        useFontCache: true,             // use <defs> and <use> in svg output? [Not Supported]
-        useGlobalCache: true,           // use common <defs> for all equations? [Not Supported]
-        linebreaks: false,              // do linebreaking? [Supported]
-        equationNumbers: "none",        // or "AMS" or "all" [Supported]
-        singleDollars: true,            // allow single-dollar delimiter for inline TeX? [Supported]
 
+    var error_html = "<h1 style='width: 100%;text-align:center;font-family:Arial,sans-serif;'>If you're seeing this, a major error has occurred. Please file an issue at the <a style='color:#67B1E5;' href='https://github.com/cemrajc/gulp-mathjax-node/issues'><b>gulp-mathjax-node</b></a> Github page!</h1>";
+
+    var defaults = {
         html: error_html,               // the HTML snippet to process, which will be overwritten in this
                                         // object by the contents of the file itself,
                                         // so it has an error message in a string.
 
-        xmlns: "mml",                   // the namespace to use for MathML [Not Supported]
-        inputs: ["AsciiMath","TeX","MathML"],  // the inputs formats to support [Not Supported]
+        linebreaks: false,              // do linebreaking? [Supported]
+        equationNumbers: "none",        // or "AMS" or "all" [Supported]
+        singleDollars: true,            // allow single-dollar delimiter for inline TeX? [Supported]
+
         renderer: "SVG",                // the output format [Supported]
                                         //    ("SVG", "NativeMML", "IMG", or "None")
-        dpi: 144,                       // dpi for png image [Not Supported - Requires Balik or somthing]
 
         addPreview: false,              // turn turn into a MathJax preview, and keep the jax [Supported]
         removeJax: true,                // remove MathJax <script> tags? [Supported]
 
+        ex: 6,                          // ex-size in pixels [Not Supported]
+        width: 1,                       // width of container (in ex) for linebreaking and tags [Not Supported]
+        useFontCache: true,             // use <defs> and <use> in svg output? [Not Supported]
+        useGlobalCache: true,           // use common <defs> for all equations? [Not Supported]
+        xmlns: "mml",                   // the namespace to use for MathML [Not Supported]
+        inputs: ["AsciiMath","TeX","MathML"],  // the inputs formats to support [Not Supported]
+        dpi: 144,                       // dpi for png image [Not Supported - Requires Balik or somthing]
         speakText: false,               // add spoken annotations to svg output? [Not Supported]
         speakRuleset: "mathspeak",      // set speech ruleset (default (chromevox rules), mathspeak) [Not Supported]
         speakStyle: "default",          // set speech style (mathspeak:  default, brief, sbrief) [Not Supported]
-
         timeout: 60 * 1000,             // 60 second timeout before restarting MathJax [Not Supported]
-    }
+    };
+
+    var options = _.defaults(_.pick(_options, defaults), defaults);
+
+    return options;
+}
+
+function renderHTML(_options) {
+
+    var options = parseOptions(_options);
 
     var stream = through.obj(function(file, enc, cb){
         if (file.isBuffer()) {
             var doc = splitHeadBodyTail(file.contents.toString());
             options.html = doc.body;
+            math.start();
+
             math.typeset(options, (result) => {
                 if (!result.errors) {
                     file.contents = new Buffer(doc.head + result.html + doc.tail);
@@ -111,15 +126,6 @@ function renderHTML(_options) {
     });
 
     return stream;
-}
-
-
-function renderHTMLLogErrors(){
-    try {
-        return renderHTML()
-    } catch(e) {
-        gutil.log(e.message);
-    }
 }
 
 
--- test/main.js
+++ test/main.js
@@ -1,4 +1,6 @@
-var math = require("../index.js"),
+var math   = require("../index.js"),
+    fs     = require("fs"),
+    gulp   = require("gulp"),
     expect = require("chai").expect;
 
 
@@ -10,81 +12,141 @@ var math = require("../index.js"),
  */
 
 
+function checkEqual(path1, path2) {
+    try {
+        file1 = fs.readFileSync(path1, {encoding: "utf-8"});
+        file2 = fs.readFileSync(path2, {encoding: "utf-8"});
+        return (file1 === file2);
+    } catch(e) {
+        console.log("> Could not compare files: ", e.code, e.path);
+        return false;
+    }
+}
 
-describe("Renderer:", function(){
-    describe("SVG", function(){
-        it('should render the proper inline SVG', function() {
-            expect(false).to.be(true);
+function testImplementation(fixture, options, cb) {
+    var file_paths = {
+        src: "fixtures/",
+        exp: "expected/",
+        tmp: "temp/"
+    }
+
+    gulp.src(file_paths.src + fixture)
+        .pipe(math(options))
+        .pipe(gulp.dest(file_paths.tmp))
+        .on('end', function() {
+            expect(checkEqual(file_paths.exp + fixture, file_paths.tmp + fixture)).to.equal(true);
+            try {
+                // fs.unlinkSync(file_paths.tmp + fixtures.svg_inline);
+            } catch(e) { }
+            cb();
         })
-        it('should render the proper display SVG', function() {
-            expect(false).to.be(true);
+}
+
+var fixtures = {
+    svg_inline: "renderer-svg-inline.html",
+    svg_display: "renderer-svg-display.html",
+
+    mathml_inline: "renderer-mathml-inline.html",
+    mathml_display: "renderer-mathml-display.html",
+
+    invalid_opts_obj: "options-default.html",
+    default_options:  "options-default.html",
+
+    no_body:  "no-body.html",
+    no_head:  "no-head.html",
+    no_html:  "no-html.html",
+    no_math:  "no-math.html",
+    no_anything:  "no-anything.html",
+
+    invalid_tex:  "invalid-tex.html",
+    mixed_invalid_tex:  "mixed-invalid-tex.html"
+}
+
+// describe("Renderers:", function() {
+//     describe("SVG", function() {
+//         it('should render the proper inline SVG', function(callback) {
+//             testImplementation(fixtures.svg_inline, { renderer: "SVG" }, callback);
+//         })
+//         it('should render the proper display SVG', function(callback) {
+//             testImplementation(fixtures.svg_display, { renderer: "SVG" }, callback);
+//         })
+//     })
+//     describe("MathML (NativeMML)", function() {
+//         it('should render the proper inline MML', function(callback) {
+//             testImplementation(fixtures.mathml_inline, { renderer: "NativeMML" }, callback);
+//         })
+//         it('should render the proper display MML', function(callback) {
+//             testImplementation(fixtures.mathml_display, { renderer: "NativeMML" }, callback);
+//         })
+//     })
+// })
+
+describe("Graceful Failures for Gulp implementation:", function() {
+    describe("Invalid options object", function() {
+        it('should just use what options it can make sense of', function(callback) {
+            // TODO: Make it emit a non-blocking warning about the "unknown" properties.
+            testImplementation(fixtures.invalid_opts_obj, {
+                cream: "corn",
+                human: "resources",
+                isValid: false
+            }, callback);
         })
     })
-    describe("MathML", function(){
-        it('should render the proper inline MML', function() {
-            expect(false).to.equal(true);
-        })
-        it('should render the proper display MML', function() {
-            expect(false).to.equal(true);
+    describe("Invalid options type", function() {
+        it('should throw an error', function(callback) {
+            try {
+                testImplementation(fixtures.default_options, "An 'options' string.", callback);
+            } catch(e) {
+                callback();
+                return true;
+            }
+            throw new Error("Should thow an error about types, but it didn't");
         })
     })
 })
 
-describe("Graceful Failures for Gulp implementation:", function(){
-    describe("Invalid options object", function(){
-        it('should emit an error and use the defaults', function(){
-            expect(false).to.equal(true);
-        })
-    })
-    describe("Invalid options type", function(){
-        it('should emit an error and use the defaults', function(){
-            expect(false).to.equal(true);
-        })
-    })
-})
 
-
-describe("Graceful Failures for Input File:", function(){
-    describe("No body tag", function(){
-        it('should render the proper SVG as normal', function() {
-            expect(false).to.equal(true);
-        })
-        it('should render the proper MathML as normal', function() {
-            expect(false).to.equal(true);
-        })
-    })
-    describe("No head tag", function(){
-        it('should render the proper SVG as normal', function() {
-            expect(false).to.equal(true);
-        })
-        it('should render the proper MathML as normal', function() {
-            expect(false).to.equal(true);
-        })
-    })
-    describe("No structuring HTML at all", function(){
-        it('should render the proper SVG as normal', function() {
-            expect(false).to.equal(true);
-        })
-        it('should render the proper MathML as normal', function() {
-            expect(false).to.equal(true);
-        })
-    })
-    describe("Invalid TeX", function(){
-        it('should emit an error and move on', function() {
-            expect(false).to.equal(true);
-        })
-        it('should still render valid math', function() {
-            expect(false).to.equal(true);
-        })
-    })
-    describe("Only standard HTML (no math)", function(){
-        it('should not touch the file', function() {
-            expect(false).to.equal(true);
-        })
-    })
-    describe("Nothing at all (no math)", function(){
-        it('should not touch the file', function() {
-            expect(false).to.equal(true);
-        })
-    })
-})
+// describe("Graceful Failures for Input File:", function() {
+//     describe("No body tag", function() {
+//         it('should render the proper SVG as normal', function(callback) {
+//             testImplementation(fixtures.no_body, { renderer: "SVG" }, callback);
+//         })
+//         it('should render the proper MathML as normal', function(callback) {
+//             testImplementation(fixtures.no_body, { renderer: "NativeMML" }, callback);
+//         })
+//     })
+//     describe("No head tag", function() {
+//         it('should render the proper SVG as normal', function(callback) {
+//             testImplementation(fixtures.no_head, { renderer: "SVG" }, callback);
+//         })
+//         it('should render the proper MathML as normal', function(callback) {
+//             testImplementation(fixtures.no_head, { renderer: "NativeMML" }, callback);
+//         })
+//     })
+//     describe("No structuring HTML at all", function() {
+//         it('should render the proper SVG as normal', function(callback) {
+//             testImplementation(fixtures.no_html, { renderer: "SVG" }, callback);
+//         })
+//         it('should render the proper MathML as normal', function(callback) {
+//             testImplementation(fixtures.no_html, { renderer: "NativeMML" }, callback);
+//         })
+//     })
+//     describe("Invalid TeX", function() {
+//         it('should emit an error and move on', function(callback) {
+//             testImplementation(fixtures.invalid_tex, { renderer: "SVG" }, callback);
+//         })
+//         it('should still render the valid math', function(callback) {
+//             testImplementation(fixtures.mixed_invalid_tex, { renderer: "SVG" }, callback);
+//         })
+//     })
+//     describe("Only standard HTML (no math)", function() {
+//         it('should not touch the file', function(callback) {
+//             testImplementation(fixtures.no_math, { renderer: "SVG" }, callback);
+//         })
+//     })
+//     describe("Nothing at all (no math)", function() {
+//         it('should not touch the file', function(callback) {
+//             testImplementation(fixtures.no_anything, { renderer: "SVG" }, callback);
+//         })
+//     })
+// })
